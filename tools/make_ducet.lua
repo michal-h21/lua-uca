255a8a2a66a91588e2f2786c17bb6960704f3c43
Michal Hoftich <michal.h21@gmail.com>
2019-06-18 22:08:55 +0200

    tool to make ducet

---
+++ tools/make_ducet.lua
@@ -0,0 +1,9 @@
+local collation = require "tools.collation"
+local serpent = require "serpent"
+
+local collator = collation.new()
+local ducet_file = io.open("data/allkeys.txt", "r")
+local content = ducet_file:read("*all")
+collator:load_ducet(content)
+ducet_file:close()
+print(serpent.dump(collator.keys))
