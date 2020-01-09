5b6ec36e086a51e5747bfce330b9bb71bb844b9d
Michal Hoftich <michal.h21@gmail.com>
2020-01-10 00:03:59 +0100

    Added German

--- src/lua-uca/lua-uca-languages.lua
+++ src/lua-uca/lua-uca-languages.lua
@@ -2,6 +2,8 @@ local languages = {}
 local tailoring_table = {1, 0, 0}
 local secondary_tailoring = {0, 1, 0}
 local tertiary_tailoring = {0, 0, 1}
+local equal_tailoring= {0, 0, 0}
+
 
 local function tailor_string(collator_obj, str)
   -- cupport the cldr strings in the form: &D<<đ<<<Đ<<ð<<<Ð
@@ -16,9 +18,10 @@ local function tailor_string(collator_obj, str)
   local function tailor_primary(a,b) tailor(a,b, tailoring_table) end
   local function tailor_secondary(a,b) tailor(a,b, secondary_tailoring) end
   local function tailor_tertiary(a,b) tailor(a,b, tertiary_tailoring) end
-  local functions = {["<"] = tailor_primary, ["<<"] = tailor_secondary, ["<<<"] = tailor_tertiary}
+  local function tailor_equal(a,b) tailor(a,b, equal_tailoring) end
+  local functions = {["<"] = tailor_primary, ["<<"] = tailor_secondary, ["<<<"] = tailor_tertiary, ["="] = tailor_equal}
   local first = str:match("^&?([^%<]+)")
-  for fn, second in str:gmatch("([<]+)([^<]+)") do
+  for fn, second in str:gmatch("([<=]+)([^<]+)") do
     local exec = functions[fn]
     exec(first, second)
     first = second -- set the current second object as first for the next round
@@ -29,7 +32,7 @@ end
 
 languages.cs = function(collator_obj)
   local tailoring = function(s) tailor_string(collator_obj, s) end
-	tailoring "&c<č<<<Č"
+  tailoring "&c<č<<<Č"
   tailoring "&h<ch<<<cH<<<Ch<<<CH"
   tailoring "&R<ř<<<Ř"
   tailoring "&s<š<<<Š"
@@ -39,13 +42,44 @@ end
 
 languages.no = function(collator_obj)
   local tailoring = function(s) tailor_string(collator_obj, s) end
+  collator_obj:uppercase_first()
   tailoring("&D<<đ<<<Đ<<ð<<<Ð")
-	tailoring("&th<<<þ")
-	tailoring("&TH<<<Þ")
-	tailoring("&Y<<ü<<<Ü<<ű<<<Ű")
-	tailoring("&ǀ<æ<<<Æ<<ä<<<Ä<ø<<<Ø<<ö<<<Ö<<ő<<<Ő<å<<<Å<<<aa<<<Aa<<<AA")
-	tailoring("&oe<<œ<<<Œ")
+  tailoring("&th<<<þ")
+  tailoring("&TH<<<Þ")
+  tailoring("&Y<<ü<<<Ü<<ű<<<Ű")
+  tailoring("&ǀ<æ<<<Æ<<ä<<<Ä<Ø<<<ø<<ö<<<Ö<<ő<<<Ő<å<<<Å<<<aa<<<Aa<<<AA")
+  tailoring("&oe<<œ<<<Œ")
   return collator_obj
 end
 
-return languages
+languages.de = function(collator_obj)
+  local tailoring = function(s) tailor_string(collator_obj, s) end
+  tailoring "&th<<<þ"
+  tailoring "&TH<<<Þ"
+  tailoring "&Y<<ƿ<<<Ƿ<<ʒ<<<Ʒ<<<ᶾ"
+  tailoring "&A<<ä<<<Ä<<ǟ<<<Ǟ<<á<<<Á<<à<<<À<<ă<<<Ă<<ắ<<<Ắ<<ằ<<<Ằ<<ẵ<<<Ẵ<<ẳ<<<Ẳ<<â<<<Â<<ấ<<<Ấ<<ầ<<<Ầ<<ẫ<<<Ẫ<<ẩ<<<Ẩ<<ǎ<<<Ǎ<<å<<<Å=Å<<ǻ<<<Ǻ<<ã<<<Ã<<ȧ<<<Ȧ<<ǡ<<<Ǡ<<ą<<<Ą<<ā<<<Ā<<ả<<<Ả<<ȁ<<<Ȁ<<ȃ<<<Ȃ<<ạ<<<Ạ<<ặ<<<Ặ<<ậ<<<Ậ<<ḁ<<<Ḁ<<ᷓ<<ᴀ<<ⱥ<<<Ⱥ<<ᶏ<<ɐ<<<Ɐ<<<ᵄ<<ɑ<<<Ɑ<<<ᵅ<<ᶐ<<ɒ<<<Ɒ<<<ᶛ"
+  tailoring "&O<<ö<<<Ö<<ȫ<<<Ȫ<<ó<<<Ó<<ò<<<Ò<<ŏ<<<Ŏ<<ô<<<Ô<<ố<<<Ố<<ồ<<<Ồ<<ỗ<<<Ỗ<<ổ<<<Ổ<<ǒ<<<Ǒ<<ő<<<Ő<<õ<<<Õ<<ṍ<<<Ṍ<<ṏ<<<Ṏ<<ȭ<<<Ȭ<<ȯ<<<Ȯ<<ȱ<<<Ȱ<<ø<<<Ø<<ǿ<<<Ǿ<<ǫ<<<Ǫ<<ǭ<<<Ǭ<<ō<<<Ō<<ṓ<<<Ṓ<<ṑ<<<Ṑ<<ỏ<<<Ỏ<<ȍ<<<Ȍ<<ȏ<<<Ȏ<<ơ<<<Ơ<<ớ<<<Ớ<<ờ<<<Ờ<<ỡ<<<Ỡ<<ở<<<Ở<<ợ<<<Ợ<<ọ<<<Ọ<<ộ<<<Ộ<<ᴏ<<ᴑ<<ᴓ<<ɔ<<<Ɔ <<<ᵓ<<ᴐ<<ᴒ<<ᶗ<<ꝍ<<<Ꝍ<<ᴖ<<<ᵔ<<ᴗ<<<ᵕ<<ⱺ<<ɵ<<<Ɵ<<<ᶱ<<ꝋ<<<Ꝋ<<ɷ<<ȣ<<<Ȣ<<<ᴽ<<ᴕ				  "
+  tailoring "&U<<ü<<<Ü<<ǘ<<<Ǘ<<ǜ<<<Ǜ<<ǚ<<<Ǚ<<ǖ<<<Ǖ<<ú<<<Ú<<ù<<<Ù<<ŭ<<<Ŭ<<û<<<Û<<ǔ<<<Ǔ<<ů<<<Ů<<ű<<<Ű<<ũ<<<Ũ<<ṹ<<<Ṹ<<ų<<<Ų<<ū<<<Ū<<ṻ<<<Ṻ<<ủ<<<Ủ<<ȕ<<<Ȕ<<ȗ<<<Ȗ<<ư<<<Ư<<ứ<<<Ứ<<ừ<<<Ừ<<ữ<<<Ữ<<ử<<<Ử<<ự<<<Ự<<ụ<<<Ụ<<ṳ<<<Ṳ<<ṷ<<<Ṷ<<ṵ<<<Ṵ<<ᴜ<<<ᶸ<<ᴝ<<<ᵙ<<ᴞ<<ʉ<<<Ʉ<<<ᶶ<<ᵾ<<ᶙ<<ʊ<<<Ʊ<<<ᶷ<<ᵿ"
+  return collator_obj
+end
+
+languages.de_din2 = function(collator_obj)
+  local function tailor(a, b, tbl) 
+    local autf = collator_obj:string_to_codepoints(a)
+    local butf = collator_obj:string_to_codepoints(b)
+    collator_obj:tailor(autf,butf, tbl) 
+  end
+  local tailoring = function(s) tailor_string(collator_obj, s) end
+  languages.de(collator_obj)
+  -- tailoring "Ö=Oe"
+  -- tailoring "ö=oe"
+
+  -- this doesn't really work
+  tailor("Ö", "Oe", {0, 0, 0})
+  tailor("ö", "oe", {0, 0, 0})
+  return collator_obj
+end
+
+
+
+  return languages
