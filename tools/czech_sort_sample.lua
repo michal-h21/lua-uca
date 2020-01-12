2f10adc47e9dc8f7015c92f339dcd99bf7e10746
Michal Hoftich <michal.h21@gmail.com>
2020-01-12 23:01:56 +0100

    Use tailoring dsl

--- tools/czech_sort_sample.lua
+++ tools/czech_sort_sample.lua
@@ -2,23 +2,32 @@ package.path = package.path .. ";src/?.lua"
 -- read text from stdin, separate words and sort them
 local ducet = require "lua-uca.lua-uca-ducet"
 local collator = require "lua-uca.lua-uca-collator"
+local languages = require "lua-uca.lua-uca-languages"
+
 local collator_obj = collator.new(ducet)
-local h = collator_obj:string_to_codepoints("h")
-local ch = collator_obj:string_to_codepoints("ch")
-local c = collator_obj:string_to_codepoints("c")
-local ccaron = collator_obj:string_to_codepoints("č")
-local r = collator_obj:string_to_codepoints("r")
-local rcaron = collator_obj:string_to_codepoints("ř")
-local s = collator_obj:string_to_codepoints("s")
-local scaron = collator_obj:string_to_codepoints("š")
-local z = collator_obj:string_to_codepoints("z")
-local zcaron = collator_obj:string_to_codepoints("ž")
-local tailoring_table = {1, 0, 0}
-collator_obj:tailor(h, ch, tailoring_table)
-collator_obj:tailor(c, ccaron, tailoring_table)
-collator_obj:tailor(r, rcaron, tailoring_table)
-collator_obj:tailor(s, scaron, tailoring_table)
-collator_obj:tailor(z, zcaron, tailoring_table)
+local tailoring = function(s) languages.tailor_string(collator_obj, s) end
+
+tailoring "&c<č<<<Č"
+tailoring "&h<ch<<<cH<<<Ch<<<CH"
+tailoring "&R<ř<<<Ř"
+tailoring "&s<š<<<Š"
+tailoring "&z<ž<<<Ž"
+-- local h = collator_obj:string_to_codepoints("h")
+-- local ch = collator_obj:string_to_codepoints("ch")
+-- local c = collator_obj:string_to_codepoints("c")
+-- local ccaron = collator_obj:string_to_codepoints("č")
+-- local r = collator_obj:string_to_codepoints("r")
+-- local rcaron = collator_obj:string_to_codepoints("ř")
+-- local s = collator_obj:string_to_codepoints("s")
+-- local scaron = collator_obj:string_to_codepoints("š")
+-- local z = collator_obj:string_to_codepoints("z")
+-- local zcaron = collator_obj:string_to_codepoints("ž")
+-- local tailoring_table = {1, 0, 0}
+-- collator_obj:tailor(h, ch, tailoring_table)
+-- collator_obj:tailor(c, ccaron, tailoring_table)
+-- collator_obj:tailor(r, rcaron, tailoring_table)
+-- collator_obj:tailor(s, scaron, tailoring_table)
+-- collator_obj:tailor(z, zcaron, tailoring_table)
 
 local words = {}
 
