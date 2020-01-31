891ccf18b8d141bf4cd6301ec7e2d57650c47746
Michal Hoftich <michal.h21@gmail.com>
2020-01-31 17:23:30 +0100

    Added collator_obj:reorder test

--- spec/tailoring-spec.lua
+++ spec/tailoring-spec.lua
@@ -1,9 +1,10 @@
-local newtable = require "lua-uca.lua-uca-reordering-table"
+local reordering_table = require "lua-uca.lua-uca-reordering-table"
 local tailoring = require "lua-uca.lua-uca-tailoring"
 local collator_lib = require "lua-uca.lua-uca-collator"
 local ducet = require "lua-uca.lua-uca-ducet"
 local reorder = tailoring.reorder
 local reorder_collator = tailoring.reorder_collator
+local newtable = collator_lib.copy_table(reordering_table)
 
 -- test sort
 local collator_obj = collator_lib.new(ducet)
@@ -15,7 +16,9 @@ describe("Test reordering", function()
     table.sort(sort_table, function(a,b) return collator_obj:compare_strings(a,b) end) 
   end
   local assert_order = function(t1, t2)
+    print("**********")
     for i = 1, #t1 do
+      print(t1[i], t2[i])
       assert.equals(t1[i], t2[i])
     end
   end
@@ -40,5 +43,11 @@ describe("Test reordering", function()
     sort(sort_table)
     -- digits should be sorted as last
     assert_order(sort_table, {"Первая",  "ahoj", "12"})
+  end)
+  it("should support collator_obj:reorder function", function()
+    local newcollator = collator_lib.new(ducet)
+    newcollator:reorder {"others", "digits"}
+    table.sort(sort_table, function(a,b) return newcollator:compare_strings(a,b) end) 
+    assert_order(sort_table, {"ahoj",  "Первая", "12"})
   end)
 end)
